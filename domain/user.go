471eef60d480007e7cd2b1bf1a7395984e51c3fe
kimcodec <kuban2545@gmail.com>
2024-03-24 18:31:58 +0300

    fix validation

--- domain/user.go
+++ domain/user.go
@@ -2,7 +2,7 @@ package domain
 
 import (
 	"github.com/go-playground/validator/v10"
-	"regexp"
+	"unicode"
 )
 
 type User struct {
@@ -15,29 +15,46 @@ type UserAuthorizeRequest struct {
 	Password string `json:"password" validate:"required"`
 }
 
-// TODO: fix validation
-
 type UserRegisterRequest struct {
-	Login    string `json:"login" validate:"required"`
-	Password string `json:"password" validate:"required"`
+	Login    string `json:"login" validate:"required,min=4,max=50"`
+	Password string `json:"password" validate:"required,isDifficultPassword"`
 }
 
 func isDifficultPassword(fldLvl validator.FieldLevel) bool {
-	// Minimum eight characters, at least one uppercase letter, one lowercase letter and one number
-	reg := regexp.MustCompile("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])[a-zA-Z0-9]{8,}$")
-	pass, ok := fldLvl.Field().Interface().(string)
-	if !ok {
-		return false
+	// Minimum eight characters, at least one uppercase letter, one lowercase letter and one number, one symbol
+	var (
+		hasMinLen  = false
+		hasUpper   = false
+		hasLower   = false
+		hasNumber  = false
+		hasSpecial = false
+	)
+	pass := fldLvl.Field().String()
+	if len(pass) >= 8 {
+		hasMinLen = true
 	}
-	return reg.MatchString(pass)
+	for _, char := range pass {
+		switch {
+		case unicode.IsUpper(char):
+			hasUpper = true
+		case unicode.IsLower(char):
+			hasLower = true
+		case unicode.IsNumber(char):
+			hasNumber = true
+		case unicode.IsPunct(char) || unicode.IsSymbol(char):
+			hasSpecial = true
+		}
+	}
+	return hasMinLen && hasUpper && hasLower && hasNumber && hasSpecial
 }
 
 func IsValid[T UserRegisterRequest | UserAuthorizeRequest](t T) error {
 	validate := validator.New()
+	validate.RegisterValidation("isDifficultPassword", isDifficultPassword)
 	err := validate.Struct(t)
 	if err != nil {
 		// TODO: parse valid errors
-		// validate := validator.New(validator.WithRequiredStructEnabled())
+		// validationErrors := err.(validator.ValidationErrors)
 		return err
 	}
 	return nil
